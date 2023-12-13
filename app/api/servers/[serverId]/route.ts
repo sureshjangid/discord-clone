5eca936ffe6dbed3be88761ce07ef03cc435fdad
suresh jangid <sureshjangir902@gmail.com>
2023-12-13 07:19:54 +0530

    update the server setting

---
+++ app/api/servers/[serverId]/route.ts
@@ -0,0 +1,30 @@
+import { currentProfile } from "@/lib/currentProfile";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    const { name, imageUrl } = await req.json();
+    if (!profile) {
+      return new NextResponse("unauthorized", { status: 401 });
+    }
+    const server = await db.server.update({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+      data: {
+        name,
+        imageUrl,
+      },
+    });
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[serverId patch]", error);
+    return new NextResponse("internal error", { status: 500 });
+  }
+}
